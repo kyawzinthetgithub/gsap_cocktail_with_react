9c4948f16dd8c90a8cbbb854d024adf2c36962b7
kyawzinthetgithub <kyawzinthetpersonal@gmail.com>
2025-08-15 23:31:31 +0630

    add assets and navbar section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import { ScrollTrigger, SplitText } from 'gsap/all';
-import gsap from 'gsap';
+import React from "react";
+import { ScrollTrigger, SplitText } from "gsap/all";
+import gsap from "gsap";
+import Navbar from "./components/Navbar";
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
-    return (
-        <div className='flex-center h-[100vh]'>
-            <h1 className='text-indigo-300'>Hello  GSAP with React</h1>
-        </div>
-    )
-}
+  return (
+    <main>
+      <Navbar />
+    </main>
+  );
+};
 
 export default App;
