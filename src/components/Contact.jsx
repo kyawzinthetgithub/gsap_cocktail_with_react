6dd2c89087bdd54f3f54196ab0cbf60fc176dfcd
kyawzinthetgithub <kyawzinthetpersonal@gmail.com>
2025-09-02 23:10:13 +0630

    footer

---
+++ src/components/Contact.jsx
@@ -0,0 +1,95 @@
+import { useGSAP } from "@gsap/react";
+import { openingHours, socials } from "../../constants";
+import { SplitText } from "gsap/all";
+import gsap from "gsap";
+
+const Contact = () => {
+  useGSAP(() => {
+    const titleSplit = SplitText.create("#contact h2", { type: "words" });
+
+    const timeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#contact",
+        start: "top center",
+      },
+      ease: "power1.inOut",
+    });
+
+    timeline
+      .from(titleSplit.words, {
+        opacity: 0,
+        yPercent: 100,
+        stagger: 0.02,
+      })
+      .from("#contact h3, #contact p", {
+        opacity: 0,
+        yPercent: 100,
+        stagger: 0.02,
+      })
+      .to("#f-left-leaf", { y: "-50", duration: 1, ease: "power1.inOut" })
+      .to(
+        "#f-right-leaf",
+        { y: "-50", duration: 1, ease: "power1.inOut" },
+        "<"
+      );
+  });
+  return (
+    <>
+      <footer id="contact">
+        <img
+          src="/images/footer-right-leaf.png"
+          alt="leaf-right"
+          id="f-right-leaf"
+        />
+        <img
+          src="/images/footer-left-leaf.png"
+          alt="leaf-left"
+          id="f-left-leaf"
+        />
+
+        <div className="content">
+          <h2 className="capitalize">Where to find us</h2>
+
+          <div>
+            <h3 className="capitalize">visit our bar</h3>
+            <p>456, Raq Blvd. #404, Los Angeles, CA 90210</p>
+          </div>
+
+          <div>
+            <h3 className="capitalize">Contact us</h3>
+            <p>(555) 987-6543</p>
+            <p>hello@jsmcocktail.com</p>
+          </div>
+
+          <div>
+            <h3 className="capitalize">open every day</h3>
+            {openingHours.map((time) => (
+              <p key={time.day}>
+                {time.day} : {time.time}
+              </p>
+            ))}
+          </div>
+
+          <div>
+            <h3 className="capitalize">social</h3>
+            <div className="flex-center gap-5">
+              {socials.map((social) => (
+                <a
+                  href={social.url}
+                  key={social.name}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  aria-label={social.name}
+                >
+                  <img src={social.icon} alt="" />
+                </a>
+              ))}
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Contact;
